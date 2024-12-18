57a2603a1121cb61bb17548d43342853984527a9
sar9494 <batbayarsaruul0@gmail.com>
2024-12-18 20:37:34 +0800

    auygf

---
+++ start.js
@@ -0,0 +1,24 @@
+const startElement = document.getElementById("start");
+const startPage = document.createElement("div");
+startPage.id = "container";
+
+const gameName = document.createElement("h1");
+gameName.innerText = "PONG";
+gameName.id = "gameName";
+
+startPage.appendChild(gameName);
+startElement.appendChild(startPage);
+
+const onePlayerButton=document.createElement("button")
+const twoPlayerButton=document.createElement("button")
+onePlayerButton.className="playerNumber"
+twoPlayerButton.className="playerNumber"
+
+
+const logIn = document.createElement("div");
+logIn.className = "login";
+const headerLogin = document.createElement("h3");
+headerLogin.id = "gameName";
+headerLogin.innerText = "LOGIN"
+logIn.appendChild(headerLogin)
+startPage.appendChild(logIn);
