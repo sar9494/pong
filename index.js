c3f9a0e0574b8d0e6e4d23686d8dc9f24655ab59
sar9494 <batbayarsaruul0@gmail.com>
2024-12-26 18:26:11 +0800

    fafe

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ const scoreText = document.querySelector("#scoreText");
 const resetBtn = document.querySelector("#resetBtn");
 const gameWidth = gameBoard.width;
 const gameHeight = gameBoard.height;
-const boardBackground = "forestgreen";
+const boardBackground = "black";
 const paddle1Color = "lightblue";
 const paddle2Color = "red";
 const paddleBorder = "black";
-const ballColor = "yellow";
-const ballBorderColor = "black";
+const ballColor = "blue"; // Ball color is now blue
+const shadowColor = "red"; // Shadow color is now red
 const ballRadius = 12.5;
-const paddleSpeed = 50;
 let intervalID;
 let ballSpeed;
 let ballX = gameWidth / 2;
@@ -20,29 +19,40 @@ let ballXDirection = 0;
 let ballYDirection = 0;
 let player1Score = 0;
 let player2Score = 0;
+let paddleMargin = 10;
 let paddle1 = {
-    width: 25,
+
+    width: 10,
     height: 100,
-    x: 0,
+    x: paddleMargin,
     y: 0
 };
 let paddle2 = {
-    width: 25,
+    width: 10,
     height: 100,
-    x: gameWidth - 25,
+    x: gameWidth - paddleMargin -10,
     y: gameHeight - 100
 };
 
+/* window.addEventListener("keydown", changeDirection); */
+
+// Trail array to store previous ball positions
+
+let ballTrail = [];
+
 window.addEventListener("keydown", changeDirection);
+
 resetBtn.addEventListener("click", resetGame);
 
 gameStart();
 
-function gameStart(){
-    createBall();
-    nextTick();
-};
-function nextTick(){
+function gameStart() {
+  createBall();
+  nextTick();
+  drawBlackHole(0,gameHeight/2)
+}
+
+function nextTick() {
     intervalID = setTimeout(() => {
         clearBoard();
         drawPaddles();
@@ -50,134 +60,244 @@ function nextTick(){
         drawBall(ballX, ballY);
         checkCollision();
         nextTick();
-    }, 10)
-};
-function clearBoard(){
-    ctx.fillStyle = boardBackground;
-    ctx.fillRect(0, 0, gameWidth, gameHeight);
-};
-function drawPaddles(){
-    ctx.strokeStyle = paddleBorder;
+    }, 10);
+}
 
-    ctx.fillStyle = paddle1Color;
-    ctx.fillRect(paddle1.x, paddle1.y, paddle1.width, paddle1.height);
-    ctx.strokeRect(paddle1.x, paddle1.y, paddle1.width, paddle1.height);
+function clearBoard() {
+  ctx.fillStyle = boardBackground;
+  ctx.fillRect(0, 0, gameWidth, gameHeight);
+}
 
-    ctx.fillStyle = paddle2Color;
-    ctx.fillRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
-    ctx.strokeRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
-};
-function createBall(){
-    ballSpeed = 1;
-    if(Math.round(Math.random()) == 1){
-        ballXDirection =  1; 
+function drawPaddles() {
+  ctx.strokeStyle = paddleBorder;
+  ctx.fillStyle = paddle1Color;
+  ctx.fillRect(paddle1.x, paddle1.y, paddle1.width, paddle1.height);
+  ctx.strokeRect(paddle1.x, paddle1.y, paddle1.width, paddle1.height);
+
+  ctx.fillStyle = paddle2Color;
+  ctx.fillRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
+  ctx.strokeRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
+}
+
+function createBall() {
+    ballSpeed = 3;
+    if (Math.round(Math.random()) == 1) {
+        ballXDirection = 1;
+    } else {
+        ballXDirection = -1;
     }
-    else{
-        ballXDirection = -1; 
-    }
-    if(Math.round(Math.random()) == 1){
-        ballYDirection = Math.random() * 1; //more random directions
-    }
-    else{
-        ballYDirection = Math.random() * -1; //more random directions
+    if (Math.round(Math.random()) == 1) {
+        ballYDirection = Math.random() * 1;
+    } else {
+        ballYDirection = Math.random() * -1;
     }
     ballX = gameWidth / 2;
     ballY = gameHeight / 2;
     drawBall(ballX, ballY);
-};
-function moveBall(){
-    ballX += (ballSpeed * ballXDirection);
-    ballY += (ballSpeed * ballYDirection);
-};
-function drawBall(ballX, ballY){
-    ctx.fillStyle = ballColor;
-    ctx.strokeStyle = ballBorderColor;
-    ctx.lineWidth = 2;
-    ctx.beginPath();
-    ctx.arc(ballX, ballY, ballRadius, 0, 2 * Math.PI);
-    ctx.stroke();
-    ctx.fill();
-};
-function checkCollision(){
-    if(ballY <= 0 + ballRadius){
-        ballYDirection *= -1;
-    }
-    if(ballY >= gameHeight - ballRadius){
-        ballYDirection *= -1;
-    }
-    if(ballX <= 0){
-        player2Score+=1;
-        updateScore();
-        createBall();
-        return;
-    }
-    if(ballX >= gameWidth){
-        player1Score+=1;
-        updateScore();
-        createBall();
-        return;
-    }
-    if(ballX <= (paddle1.x + paddle1.width + ballRadius)){
-        if(ballY > paddle1.y && ballY < paddle1.y + paddle1.height){
-            ballX = (paddle1.x + paddle1.width) + ballRadius; // if ball gets stuck
-            ballXDirection *= -1;
-            ballSpeed += 1;
-        }
-    }
-    if(ballX >= (paddle2.x - ballRadius)){
-        if(ballY > paddle2.y && ballY < paddle2.y + paddle2.height){
-            ballX = paddle2.x - ballRadius; // if ball gets stuck
-            ballXDirection *= -1;
-            ballSpeed += 1;
-        }
-    }
-};
-function changeDirection(event){
-    const keyPressed = event.keyCode;
-    const paddle1Up = 87;
-    const paddle1Down = 83;
-    const paddle2Up = 38;
-    const paddle2Down = 40;
+}
 
-    switch(keyPressed){
-        case(paddle1Up):
-            if(paddle1.y > 0){
-                paddle1.y -= paddleSpeed;
-            }
-            break;
-        case(paddle1Down):
-            if(paddle1.y < gameHeight - paddle1.height){
-                paddle1.y += paddleSpeed;
-            }
-            break;
-        case(paddle2Up):
-            if(paddle2.y > 0){
-                paddle2.y -= paddleSpeed;
-            }
-            break;
-        case(paddle2Down):
-            if(paddle2.y < gameHeight - paddle2.height){
-                paddle2.y += paddleSpeed;
-            }
-            break;
+function moveBall() {
+  ballX += ballSpeed * ballXDirection;
+  ballY += ballSpeed * ballYDirection;
+}
+
+function drawBall(ballX, ballY) {
+  ballTrail.push({ x: ballX, y: ballY });
+
+  if (ballTrail.length > 20) {
+    ballTrail.shift();
+  }
+
+  for (let i = 0; i < ballTrail.length; i++) {
+    const trailAlpha = (i + 1) / ballTrail.length;
+    ctx.fillStyle = `rgba(255, 0, 0, ${trailAlpha})`; // Red shadow effect with fading
+    ctx.fillRect(
+      ballTrail[i].x - ballRadius / 2,
+      ballTrail[i].y - ballRadius / 2,
+      ballRadius,
+      ballRadius
+    );
+  }
+
+  const ballGradient = ctx.createRadialGradient(
+    ballX,
+    ballY,
+    0,
+    ballX,
+    ballY,
+    ballRadius
+  );
+  ballGradient.addColorStop(0, "rgba(255, 255, 255, 1)");
+  ballGradient.addColorStop(1, "rgba(0, 0, 255, 1)"); // Ball color gradient from white to blue
+
+  ctx.beginPath();
+  ctx.arc(ballX, ballY, ballRadius, 0, 2 * Math.PI);
+  ctx.fillStyle = ballGradient;
+  ctx.fill();
+}
+
+function checkCollision() {
+  if (ballY <= 0 + ballRadius) {
+    ballYDirection *= -1;
+    playSound("bounceSound"); 
+  }else if (ballY >= gameHeight - ballRadius) {
+    ballYDirection *= -1;
+    playSound("bounceSound"); 
+  }else if((ballX<=0+ballRadius&&ballY>gameHeight-100&&ballY<gameHeight-ballRadius)){
+    ballXDirection *= -1;
+    playSound("bounceSound"); 
+  }else if((ballX<=0+ballRadius&&ballY<100&&ballY>ballRadius)){
+    ballXDirection *= -1;
+    playSound("bounceSound"); 
+  }else if((ballX>=gameWidth-ballRadius&&ballY>gameHeight-100&&ballY<gameHeight-ballRadius)){
+    ballXDirection *=-1;
+    playSound("bounceSound"); 
+  }else if((ballX>=gameWidth-ballRadius&&ballY<100&&ballY>ballRadius)){
+    ballXDirection *=-1;
+    playSound("bounceSound"); 
+  }
+  if (ballX <= 0) {
+    player2Score += 1;
+    updateScore();
+    createBall();
+    increaseBallSpeed();
+    playSound("scoreSound");  
+    return;
+  }
+  if (ballX >= gameWidth) {
+    player1Score += 1;
+    updateScore();
+    createBall();
+    increaseBallSpeed();
+    playSound("scoreSound");  
+    return;
+  }
+  if (ballX <= paddle1.x + paddle1.width + ballRadius) {
+    if (ballY > paddle1.y && ballY < paddle1.y + paddle1.height) {
+      ballX = paddle1.x + paddle1.width + ballRadius;
+      ballXDirection *= -1;
+      playSound("bounceSound"); 
     }
-};
-function updateScore(){
-    scoreText.textContent = `${player1Score} : ${player2Score}`;
-};
-function resetGame(){
+  }
+  if (ballX >= paddle2.x - ballRadius) {
+    if (ballY > paddle2.y && ballY < paddle2.y + paddle2.height) {
+      ballX = paddle2.x - ballRadius;
+      ballXDirection *= -1;
+      playSound("bounceSound"); 
+    }
+  }
+}
+
+function increaseBallSpeed() {
+  ballSpeed += 2;
+  if (ballSpeed > 15) {
+    ballSpeed = 15;
+  }
+}
+let paddleSpeed = 8;
+let paddle1SpeedY = 0;
+let paddle2SpeedY = 0;
+
+function changeDirection(event) {
+  const keyPressed = event.keyCode;
+  const paddle1Up = 87;
+  const paddle1Down = 83;
+  const paddle2Up = 38;
+  const paddle2Down = 40;
+
+  switch (keyPressed) {
+    case paddle1Up:
+      paddle1SpeedY = -paddleSpeed;
+      break;
+    case paddle1Down:
+      paddle1SpeedY = paddleSpeed;
+      break;
+    case paddle2Up:
+      paddle2SpeedY = -paddleSpeed;
+      break;
+    case paddle2Down:
+      paddle2SpeedY = paddleSpeed;
+      break;
+  }
+}
+
+function stopPaddleMovement(event) {
+  const keyPressed = event.keyCode;
+  const paddle1Up = 87;
+  const paddle1Down = 83;
+  const paddle2Up = 38;
+  const paddle2Down = 40;
+
+  switch (keyPressed) {
+    case paddle1Up:
+    case paddle1Down:
+      paddle1SpeedY = 0;
+      break;
+    case paddle2Up:
+    case paddle2Down:
+      paddle2SpeedY = 0;
+      break;
+  }
+}
+function updatePaddlePosition(){
+    
+    if (paddle1.y + paddle1SpeedY >= 0 && paddle1.y + paddle1SpeedY <= gameHeight - paddle1.height) {
+        paddle1.y += paddle1SpeedY;
+    }
+    if (paddle2.y + paddle2SpeedY >= 0 && paddle2.y + paddle2SpeedY <= gameHeight - paddle2.height) {
+        paddle2.y += paddle2SpeedY;
+    }
+
+    requestAnimationFrame(updatePaddlePosition);
+}
+
+document.addEventListener('keydown', changeDirection);
+document.addEventListener('keyup', stopPaddleMovement);
+
+requestAnimationFrame(updatePaddlePosition);
+
+
+
+
+function updatePaddlePosition() {
+  if (
+    paddle1.y + paddle1SpeedY >= 0 &&
+    paddle1.y + paddle1SpeedY <= gameHeight - paddle1.height
+  ) {
+    paddle1.y += paddle1SpeedY;
+  }
+  if (
+    paddle2.y + paddle2SpeedY >= 0 &&
+    paddle2.y + paddle2SpeedY <= gameHeight - paddle2.height
+  ) {
+    paddle2.y += paddle2SpeedY;
+  }
+
+  requestAnimationFrame(updatePaddlePosition);
+}
+
+document.addEventListener("keydown", changeDirection);
+document.addEventListener("keyup", stopPaddleMovement);
+
+requestAnimationFrame(updatePaddlePosition);
+
+function updateScore() {
+  scoreText.textContent = `${player1Score} : ${player2Score}`;
+}
+
+function resetGame() {
     player1Score = 0;
     player2Score = 0;
     paddle1 = {
-        width: 25,
+        width: 10,
         height: 100,
-        x: 0,
+        x: paddleMargin,
         y: 0
     };
     paddle2 = {
-        width: 25,
+        width: 10,
         height: 100,
-        x: gameWidth - 25,
+        x: gameWidth - paddleMargin - 10,
         y: gameHeight - 100
     };
     ballSpeed = 1;
@@ -188,4 +308,57 @@ function resetGame(){
     updateScore();
     clearInterval(intervalID);
     gameStart();
-}; 
+}
+
+// sound
+
+function playSound(soundId) {
+    const sound = document.getElementById(soundId);
+    sound.currentTime = 0;  // Дууг эхнээс нь дахин тоглуулах
+    sound.play();
+}
+//=========================tttt
+const computerButtom = document.createElement('button')
+computerButtom.innerHTML = 'player VS computer'
+computerButtom.id= 'computerButtom'
+document.getElementById('gameContainer').appendChild(computerButtom)
+const player1VSplayer2= document.createElement('button')
+player1VSplayer2.id= 'player1VSplayer2'
+player1VSplayer2.innerHTML = 'player VS player'
+document.getElementById('gameContainer').appendChild(player1VSplayer2)
+player1VSplayer2.addEventListener('click', () => {
+    computerButtom.disabled = true;
+    window.addEventListener("keydown", changeDirection);
+  });
+computerButtom.addEventListener('click', () => {
+    player1VSplayer2.disabled = true;
+    setTimeout(() => {
+      computerFunction();
+    }, 1000);});
+
+  
+function computerFunction() {
+    document.getElementById("gameBoard").addEventListener('mousemove', (e) => {
+      
+        const mouseY = e.clientY;
+        paddle1.y = mouseY -180;
+    });
+
+   
+    setInterval(() => {
+        if (ballY < paddle2.y + paddle2.height / 2) {
+            paddle2.y -= 15; 
+        } else if (ballY > paddle2.y + paddle2.height / 2) {
+            paddle2.y += 15; 
+        }
+    }, 100);
+}
+//gay
+
+function drawBlackHole(x,y){
+ctx.beginPath();
+ctx.ellipse(x,y,50,150)
+ctx.closePath();
+ctx.fillStyle="green"
+ctx.fill()
+}
--- start.js
+++ start.js
@@ -9,31 +9,86 @@ gameName.id = "gameName";
 startPage.appendChild(gameName);
 startElement.appendChild(startPage);
 
-let player1Name="player 1"
-let player2Name="player 2"
-const buttonContainer=document.createElement("div")
-buttonContainer.className="buttonContainer"
-const onePlayerButton=document.createElement("button")
-onePlayerButton.className="playerNumber"
-onePlayerButton.innerText="one player"
+let player1Name = "player 1"
+let player2Name = "player 2"
+const buttonContainer = document.createElement("div")
+buttonContainer.className = "buttonContainer"
+const onePlayerButton = document.createElement("button")
+onePlayerButton.className = "playerNumber"
+onePlayerButton.innerText = "one player"
 
-const twoPlayerButton=document.createElement("button")
-twoPlayerButton.className="playerNumber"
-twoPlayerButton.innerText="two player"
-twoPlayerButton.addEventListener(()=>{
+const twoPlayerButton = document.createElement("button")
+twoPlayerButton.className = "playerNumber"
+twoPlayerButton.innerText = "two player"
 
-})
 
 buttonContainer.appendChild(onePlayerButton)
 buttonContainer.appendChild(twoPlayerButton)
 
 startPage.appendChild(buttonContainer);
 
-
+function loginOpen(imageUrl,inputId,imgId){
 const logIn = document.createElement("div");
 logIn.className = "login";
 const headerLogin = document.createElement("h3");
 headerLogin.id = "gameName";
 headerLogin.innerText = "LOGIN"
 logIn.appendChild(headerLogin)
-startPage.appendChild(logIn);
+let profileChosen = chosenImage(imageUrl,imgId)
+profileChosen.addEventListener("click", () => {
+  logInContainer.style.display="none"
+  const editProfile = editPro(imgId).container
+  editProfile.style.display = "flex"
+  startPage.appendChild(editProfile)
+  profileChosen=editProfile.newImg;
+})
+const nickname = document.createElement("p")
+nickname.innerText = "Nickname:"
+const nicknameInput = document.createElement("input")
+nicknameInput.type = "text"
+nicknameInput.id=inputId
+logIn.appendChild(profileChosen)
+logIn.appendChild(nickname);
+logIn.appendChild(nicknameInput)
+
+return {container:logIn,image:imageUrl}
+
+}
+const logInContainer=document.createElement("div")
+logInContainer.className = "logInContainer"
+let logInPlayer1=loginOpen("defaultProfile.jpg","nicknamePlayer1","player1img").container
+let logInPlayer2=loginOpen("defaultProfile.jpg","nicknamePlayer2","player2img").container
+logInContainer.appendChild(logInPlayer1);
+logInContainer.appendChild(logInPlayer2);
+startPage.appendChild(logInContainer)
+
+function imageChoices(imageUrl) {
+  const img = document.createElement('img');
+  img.id="image"
+  img.src = imageUrl;
+  return img;
+}
+function editPro(imgId){
+  const editProfile = document.createElement("div")
+  editProfile.id = "editProfile"
+  const profileImg = ["profile1.jpg", "profile2.jpg", "profile3.webp", "profile4.jpg",]
+  let changeSrc=document.getElementById(imgId)
+  profileImg.forEach(el => {
+    const edited=imageChoices(el)
+    editProfile.appendChild(edited)
+    edited.addEventListener("click", () => {
+      changeSrc.src=el
+      editProfile.style.display="none"
+      logInContainer.style.display="flex"
+    })
+  })
+  return {container:editProfile , newImg:changeSrc}
+}
+function chosenImage(imageUrl,imgId) {
+  const imagePro = document.createElement("img")
+  imagePro.id = imgId
+  imagePro.src = imageUrl
+  return imagePro
+}
+
+
